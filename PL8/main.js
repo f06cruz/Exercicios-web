fe7c6cacd8cf19b22b45b088c164a5204eafce9a
unknown <swd13@CESAEPRT.WAN>
2026-04-23 13:04:51 +0100

    23/4

--- PL8/main.js
+++ PL8/main.js
@@ -2,13 +2,13 @@ function ex01(){
 let nome = "Filipe"
 //alert (nome)
 console.log(nome)
-document.write(nome)
+//document.write(nome)
 }
 
 function ex02(){
 let nome = "Filipe"
 let sobrenome ="Cruz"
-document.write(nome+" "+sobrenome)
+console.log(nome+" "+sobrenome)
 }
 
 function ex03(){
@@ -16,10 +16,10 @@ let nome = "Nome: XXX"
 let sobrenome = "Sobrenome: YYY"
 let idade = "Idade: 123"
 let email = "Email: aaa@bbb.cc"
-document.write(nome+"<br>")
-document.write(sobrenome+"<br>")
-document.write(idade+"<br>")
-document.write(email+"<br>")
+console.log(nome)
+console.log(sobrenome)
+console.log(idade)
+console.log(email)
 }
 
 function ex04(){
@@ -27,10 +27,10 @@ let nome = "XXX"
 let sobrenome = "YYY"
 let idade = "123"
 let email = "aaa@bbb.cc"
-document.write("Nome: "+nome+" ("+nome.length+")<br>")
-document.write("Sobrenome: "+sobrenome+" ("+sobrenome.length+")<br>")
-document.write("Idade: "+idade+"<br>")
-document.write("Email: "+email+"<br>")
+console.log("Nome: "+nome+" ("+nome.length+")")
+console.log("Sobrenome: "+sobrenome+" ("+sobrenome.length+")")
+console.log("Idade: "+idade)
+console.log("Email: "+email)
 }
 
 function ex05(){
@@ -38,66 +38,284 @@ let nome = "Filipe"
 let sobrenome = "Cruz"
 let idade = "123"
 let email = "aaa@bbb.cc"
-document.write("Nome: "+nome.toLocaleUpperCase()+" ("+nome.length+")<br>")
-document.write("Sobrenome: "+sobrenome.toLocaleUpperCase()+" ("+sobrenome.length+")<br>")
-document.write("Idade: "+idade+"<br>")
-document.write("Email: "+email+"<br>")
+console.log("Nome: "+nome.toLocaleUpperCase()+" ("+nome.length+")<br>")
+console.log("Sobrenome: "+sobrenome.toLocaleUpperCase()+" ("+sobrenome.length+")")
+console.log("Idade: "+idade)
+console.log("Email: "+email)
 }
 
 function ex06(){
     let texto = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, ratione nostrum molestiae reprehenderit odio dolores et nulla sunt, exercitationem, autem ipsum excepturi ea neque quasi. Culpa tenetur repudiandae voluptatum perferendis."
-    document.write(texto+"<br>")
-    document.write("primeiro A: "+texto.indexOf("a")+"<br>")
-    document.write("ultimo B: "+texto.lastIndexOf("b")+"<br>")
+    console.log(texto)
+    console.log("primeiro A: "+texto.indexOf("a"))
+    console.log("ultimo B: "+texto.lastIndexOf("b"))
 
 }
 
 function ex07(){
-    let n1= Math.random()*10
-    let n2= Math.random()*10
+    let n1= Math.ceil(Math.random()*10)
+    let n2= Math.ceil(Math.random()*10)
 
     let soma = n1 + n2
     let sub = n1 - n2
     let mult = n1 * n2
     let div = n1 / n2
     let rdiv = n1 % n2
-    let exp = n1 ** n1
+    let exp = Math.pow(n1,n2) //n1**n2
     let raiz = Math.sqrt(n1)
     let absl = Math.abs(n2)
 
-    document.write("n.1: "+n1+"<br>")
-    document.write("n.2: "+n2+"<br>")
-    document.write("soma: "+soma+"<br>")
-    document.write("subtração: "+sub+"<br>")
-    document.write("multiplicação: "+mult+"<br>")
-    document.write("divisão: "+div+"<br>")
-    document.write("resto: "+rdiv+"<br>")
-    document.write("exponencial: "+exp+"<br>")
-    document.write("raiz: "+raiz+"<br>")
-    document.write("absoluto: "+absl+"<br>")
+    console.log("n.1: "+n1)
+    console.log("n.2: "+n2)
+    console.log("soma: "+soma)
+    console.log("subtração: "+sub)
+    console.log("multiplicação: "+mult)
+    console.logconsole.logconsole.log("divisão: "+div)
+    console.log("resto: "+rdiv)
+    console.log("exponencial: "+exp)
+    console.log("raiz: "+raiz)
+    console.log("absoluto: "+absl)
 }
 
 function ex08(){
-    document.write(Math.random())
+    console.log(Math.random())
 }
 
 function ex09(){
-    document.write(Math.ceil(Math.random()*10))
+    console.log(Math.ceil(Math.random()*10))
 }
 
 function ex10(){
     let n1 = Math.random()*10
     let n2 = Math.random()*10
 
-    document.write("n1: "+n1+"<br>")
-    document.write("n2: "+n2+"<br>")
-    document.write("n1 arredondado: "+n1.toFixed(2)+"<br>")
-    document.write("n2 arredondado: "+n2.toFixed(2)+"<br>")
-    document.write("n1 arredondado baixo: "+Math.ceil(Number(n1.toFixed(2)))+"<br>")
-    document.write("n2 arredondado baixo: "+Math.ceil(Number(n2.toFixed(2)))+"<br>")
-    document.write("n1 arredondado cima: "+Math.floor(Number(n1.toFixed(2)))+"<br>")
-    document.write("n2 arredondado cima: "+Math.floor(Number(n2.toFixed(2)))+"<br>")
+    console.log("n1: "+n1)
+    console.log("n2: "+n2)
+    console.log("n1 arredondado: "+n1.toFixed(2))
+    console.log("n2 arredondado: "+n2.toFixed(2))
+    console.log("n1 arredondado baixo: "+Math.floor(n1))
+    console.log("n2 arredondado baixo: "+Math.floor(n2))
+    console.log("n1 arredondado cima: "+Math.ceil(n1))
+    console.log("n2 arredondado cima: "+Math.ceil(n2))
 }
 
+function ex11(){
+    let num = Math.ceil(Math.random()*100)
 
-ex10();
+    document.write(num+"<br>")
+    if (num>37.5)
+        document.write("Tem febre")
+    else
+        document.write("Não tem febre")
+}
+
+function ex12(){
+    let num = Math.ceil(Math.random()*100)
+
+    console.log(num+"\n")
+    if (num>37.5)
+        console.log("Tem febre")
+    else {
+        if(num>35){
+        console.log("Não tem febre")}
+        else{
+            console.log("Temperatura muito baixa")}
+    }
+}
+
+function ex13(){
+    let idade= 30
+    let genero= "Homem"
+
+    if (idade<25){
+        if (genero="Homem")
+            console.log("O jovem é menor de idade")
+        else if (genero="Mulher")
+            console.log("A jovem é menor de idade")}
+    else {if (genero="Homem")
+            console.log("O homem é maior de idade")
+        else if (genero="Mulher")
+            console.log("A jovem é maior de idade")}
+
+}
+
+function ex14(){
+    let num = Math.ceil(Math.random()*10)
+
+    console.log(num)
+    if (num==1)
+        console.log("Um")
+    else if (num==2)
+        console.log("Dois")
+    else if (num==3)
+        console.log("Três")
+    else if (num==4)
+        console.log("Quatro")
+    else if (num==5)
+        console.log("Cinco")
+    else  console.log("Outro valor")
+}
+     
+function ex15(){
+    let num =Math.ceil(Math.random()*10)
+
+    console.log(num)
+    switch (num){
+        case 1:
+            console.log("Um")
+            break
+        case 2:
+            console.log("Dois")
+            break
+        case 3:
+            console.log("Três")
+            break
+        case 4:
+            console.log("Quatro")
+            break
+        case 5:
+            console.log("Cinco")
+            break
+        default: 
+            console.log("Outro valor")
+    }
+}
+
+function ex16(){
+    let cont=1
+
+    for(let x=0;x<1000;x++){
+        console.log(cont)
+        cont++
+    }
+}
+
+function ex17(){
+    let cont=1
+
+    while(cont<=1000){
+        console.log(cont)
+        cont++
+    }
+}
+
+function ex18(){
+    let n1= Math.ceil(Math.random()*10)
+    let n2= Math.ceil(Math.random()*10)
+
+    console.log(n1)
+    console.log(n2)
+
+    if(n1%n2==0)
+        console.log("Múltiplo")
+    else
+        console.log("Não Múltiplo")
+}
+
+function ex19(){
+    let n1= Math.ceil(Math.random()*10)
+
+    console.log(n1)
+
+    if(n1%2==0)
+        console.log("Par")
+    else
+        console.log("Impar")
+}
+
+function ex20(){
+    let n1= '153'
+
+
+    if(n1>99 && n1<1000){
+        console.log(n1.charAt(0)) 
+        console.log(n1.charAt(2))  
+    }  
+}
+
+function ex21_e(){
+    let num= new Array(Math.ceil(Math.random()*10),Math.ceil(Math.random()*10),Math.ceil(Math.random()*10))
+
+    console.log(num[0])
+    console.log(num[1])
+    console.log(num[2])
+
+    console.log(num.sort())
+
+}
+
+function ex21(){
+    let n1= Math.ceil(Math.random()*10)
+    let n2= Math.ceil(Math.random()*10)
+    let n3= Math.ceil(Math.random()*10)
+
+    console.log(n1)
+    console.log(n2)
+    console.log(n3)
+
+    console.log("\n"+Math.min(n1,n2,n3))
+}
+
+function ex22(){
+    let cel= Math.ceil(Math.random()*100)
+
+    console.log(cel+"C")
+    console.log((cel*1.8+32)+"F")
+}
+
+function ex23(){
+    let euro= Math.ceil(Math.random()*100)
+
+    console.log(euro.toFixed(2)+"€")
+    console.log((euro*1.2).toFixed(2)+"$")
+    console.log((euro*0.9).toFixed(2)+"£")
+    console.log((euro*4).toFixed(2)+"R$")
+    console.log((euro*28.5).toFixed(2)+"P$")
+}
+
+function ex24(){
+    let num=Math.ceil(Math.random()*100)
+
+    if(num.length%2!=0)
+        console.log(num)
+}
+
+function ex25(){
+    let num=Math.ceil(Math.random()*100)
+    let n1=0
+
+    console.log("\n"+num+"\n")
+    if(num>10)
+        for( let x=0; x<=num; x++){
+            console.log(n1)
+            n1++
+        }
+}
+
+function ex26(){
+    let num=Math.ceil(Math.random()*100)
+    let n1=2
+
+    console.log("\n"+num+"\n")
+    for( let x=2; x<=num; x++){
+            if(x%2==0)
+                console.log(n1)
+            n1++
+        }
+}
+
+function ex27(){
+    let num=Math.ceil(Math.random()*100)
+    let n1=0
+
+    console.log("\n"+num+"\n")
+    if(num%2==0&& num!=1 && num!=0 && num<100)
+        for( let x=0; x<=num; x++){
+            if(x%5==0)
+                console.log(n1)
+            n1++
+        }
+}
+            
+
+ex27();
---
+++ PL9/main.js
@@ -0,0 +1,10 @@
+
+
+function mudarcor(){
+document.body.style.backgroundColor= 'green';
+document.getElementById("autor").innerHTML =  "Filipe Cruz";
+document.getElementById("autor").style.color = 'red';
+}
+
+function mudarcor2(){
+document.body.style.backgroundColor= 'blue';}
